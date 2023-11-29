345999546ada119833c54a13c42a9589b73f9b66
vefa çağlar <noxfide@gmail.com>
2023-11-30 00:38:32 +0300

    created first components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>dark blog</h1>
+            <div className="links">
+                <a href="/">home</a>
+                <a href="/create">new blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
