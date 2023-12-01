477db36829b4d0262da93541ca4f56de96e6ab08
vefa çağlar <noxfide@gmail.com>
2023-12-01 20:21:51 +0300

    mapping list

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
 import { useState } from "react"
 
 const Home = () => {
-    const [name, setName] = useState("mario")
-    const [age, setAge] = useState(33)
-
-    const handleClick = (e) => {
-        setName("luigi")
-        setAge(34)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="home">
-            <h2>home page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>
-                click me
-            </button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
