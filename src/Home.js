e245b206668d037c4a5140a4e357489617748ca3
vefa çağlar <noxfide@gmail.com>
2023-12-02 03:52:44 +0300

    handle delete, useeffect, datajson with json server

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,9 +8,17 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const handleDelete = (id) => {
+        setBlogs(blogs.filter(x => x.id !== id))
+    }
+
+    useEffect(() => {
+        console.log("somethings changed", blogs)
+    },[])
+
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="all blogs" />
+            <BlogList blogs={blogs} title="all blogs" handleDelete={handleDelete} />
         </div>
     );
 }
