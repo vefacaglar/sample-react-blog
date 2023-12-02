ecd7aea5d449f65ab96148cd4f7476c3c754247a
vefa çağlar <noxfide@gmail.com>
2023-12-02 04:12:33 +0300

    getting data from jsonserver then fetching with conditions

--- src/Home.js
+++ src/Home.js
@@ -2,23 +2,27 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-
-    const handleDelete = (id) => {
-        setBlogs(blogs.filter(x => x.id !== id))
-    }
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
-        console.log("somethings changed", blogs)
-    },[])
+        fetch("http://localhost:8000/blogs")
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                setBlogs(data)
+                setIsPending(false)
+            })
+            .catch(() => {
+                setIsPending(false)
+            })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="all blogs" handleDelete={handleDelete} />
+            {isPending && <div>loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="all blogs" />}
         </div>
     );
 }
